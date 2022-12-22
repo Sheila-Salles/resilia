13e8d0739b04b22bca7d910d28a8b64341bebe64
joaocarlosagra <joaocarlosagra@gmail.com>
2022-12-22 14:53:06 -0300

    Update Robo.py

--- Robo.py
+++ Robo.py
@@ -2,3 +2,31 @@
 # Autor: João Carlos Agra dos Santos #
 
 # Criar um programa que
+
+print("Bem vindo a Bendito.")
+print()
+print("-"*40)
+print()
+print("Digite a opção desejada.")
+print("1 - Comercial")
+print("2 - Financeiro")
+print("3 - Suporte")
+print("4 - Sair")
+a1 = int(input())
+if (a1=1):
+    print("Ola sou Tawan.")
+    print("Que produto gostaria de adquirir?")
+    a2 = int(input())
+elif (a1=2):
+    print("Ola, sou Manuela.")
+    print("O que deseja do setor financeiro?")
+    a3 = int(input())
+elif (a1=3):
+    print("Ola, sou Artur.")
+    print("No que posso ajudar?")
+    a4 = int(input())
+elif (a1=4):
+    print("Obrigado por passar aqui.")
+else (a1):
+    print("Vamos começar novamente.")
+    
